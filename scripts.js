9d51ba7773dd139054e69357dc4e0c353d183eee
Wanndeko <camposwanderley418@gmail.com>
2023-10-04 13:01:35 -0300

    :lipstick: feat

--- scripts.js
+++ scripts.js
@@ -1,15 +1,20 @@
 const mainContainer = document.querySelector('main')
 const progressBar = document.querySelector('.progress-bar')
+const thumb = document.querySelector('.thumb-img')
 
 const bgImages = ['bg-galactus', 'cyber-bg', 'gold-bg', 'jungle-bg', 'red-bg']
-const currentWidth = ['20%','40%','60%','80%','100%']
-const intervalTime = 2000
+const currentWidth = ['20%', '40%', '60%', '80%', '100%']
+const thumbImages = ['galct', 'cyber', 'gold', 'jung', 'red']
 let indice = 0
 let targetWidth = 0
+const timeInterval = 3000
+
 
 function changeBackground() {
     mainContainer.style.backgroundImage = `url(./assets/bg-img/${bgImages[indice]}.png)`
+    thumb.src = `./assets/videos-thumbs/${thumbImages[indice]}-thumb.png`
     indice = (indice + 1) % bgImages.length
+
 }
 
 function percentBar() {
@@ -17,8 +22,9 @@ function percentBar() {
     targetWidth = (targetWidth + 1) % currentWidth.length
 }
 
-setInterval(percentBar, intervalTime)
-setInterval(changeBackground, intervalTime);
+
+setInterval(percentBar,  timeInterval)
+setInterval(changeBackground, timeInterval);
 
 
 
