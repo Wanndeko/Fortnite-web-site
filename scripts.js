9736a5795cfddff1712adde9077acc71ba96a37c
Wanndeko <camposwanderley418@gmail.com>
2023-10-04 18:03:03 -0300

    :lipstick: feat

--- scripts.js
+++ scripts.js
@@ -1,6 +1,9 @@
 const mainContainer = document.querySelector('main')
 const progressBar = document.querySelector('.progress-bar')
 const thumb = document.querySelector('.thumb-img')
+const headerColor = document.querySelector('header')
+const btnResponsive = document.querySelector('.responsive-btn')
+const menuResponsive = document.querySelector('.responsive-menu')
 
 const bgImages = ['bg-galactus', 'cyber-bg', 'gold-bg', 'jungle-bg', 'red-bg']
 const currentWidth = ['20%', '40%', '60%', '80%', '100%']
@@ -22,7 +25,24 @@ function percentBar() {
     targetWidth = (targetWidth + 1) % currentWidth.length
 }
 
+window.onscroll = () => {
+    if (window.pageYOffset > 100) {
+        headerColor.style.background = 'rgba(0, 0, 0, 0.34)'
+    } else {
+        headerColor.style.background = 'none'
+    }
+  }
 
+
+function openMenu(){
+    if(menuResponsive.style.display == 'none'){
+        menuResponsive.style.display = 'block' 
+    }else{
+        menuResponsive.style.display = 'none' 
+    }
+}
+
+btnResponsive.addEventListener('click', openMenu)
 setInterval(percentBar,  timeInterval)
 setInterval(changeBackground, timeInterval);
 
